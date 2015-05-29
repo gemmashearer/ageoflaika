7bfb8ca67869527bd8727d910ab04c92ee4c94f4
Gemma Shearer <gemmaandjude@gmail.com>
2015-05-29 22:22:22 +0100

    Solved the calculation problem but the code is disgusting and needs refactoring

--- laika/ViewController.swift
+++ laika/ViewController.swift
@@ -44,9 +44,24 @@ class ViewController: UIViewController {
         if dogsScientificAge <= 2 {
             dogsAgeLabel.text = "Your dog is " + "\(Double(dogsScientificAge) * youngConversionConstant)" + " dog years old"
         } else {
-            dogsAgeLabel.text = "Your dog is " + "\(dogsScientificAge * oldConversionConstant)" + " dog years old"
+            let dogsScientificAge = dogsAgeTextField.text.toInt()!
+            let yearsMoreThanTwo = dogsScientificAge - 2
+           // dogsAgeLabel.text = "\(yearsMoreThanTwo)"
+            var oldAge = yearsMoreThanTwo * oldConversionConstant
+            var youngAge = Double(2 * youngConversionConstant)
+            dogsAgeLabel.text = "\(Double(oldAge) + youngAge)"
+            
+           // dogsAgeLabel.text = "\((yearsMoreThanTwo * oldConversionConstant) + Double((2 * youngConversionConstant)))"
+            
+            
+            //dogsAgeLabel.text = "Your dog is " + "\((yearsOlderThan2 * oldConversionConstant) + 2 * youngConversionConstant)" + " dog years old"
         }
     }
     
 }
 
+
+/* the idea is that the first 2 years of any dog will be * 10.5 so then the next x years will be * 4. So for the else statement I need a way of calculating the difference between the age and 2 and then doing maths on it 
+so I need to create a variable which is basically the dog age - 2
+
+*/
